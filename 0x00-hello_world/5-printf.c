b5d20fdb5254ed7ed06eb11e1a2c3c12b4a5ab15
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-16 07:24:40 -0700

    new with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammer, but the outcome is a piece of art."
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
