fb45c431f6036bf5e98ad7f378354f29928fc75e
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-16 03:26:45 -0700

    new puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-/**
- * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
- *
- * return: Always 0.
- */
-int main(void)
+int main (void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
