90fd78754ddcda885be95e198fa61d9ef6b95ebf
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-16 03:08:17 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * File: 4-puts.c
+ */
+
+#include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual");
+	
+	return (0);
+}
