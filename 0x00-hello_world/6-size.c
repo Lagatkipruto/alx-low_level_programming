f9a103741f3a220166c462a743e5a7d3f21e7a81
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-16 07:45:58 -0700

    printing the size of characters

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various types based on the computer its compiled on.
+ *
+ * Retun: Always 0.
+ */
+int main(void)
+{
+	printf("Size of a char:%zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", size of(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+
+}
