63d351b4394a10acef918389bc81f9a2fa2511cf
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-16 03:20:08 -0700

    puts in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-/*
- * File: 4-puts.c
- */
+#!/bin/bash
 
 #include <stdio.h>
+
 /**
  * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
  *
  * return: Always 0.
  */
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual");
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return (0);
 }
