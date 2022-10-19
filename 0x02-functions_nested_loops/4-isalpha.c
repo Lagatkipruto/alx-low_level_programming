210f9401ed14bca9dea5012c8b061aec11dc4ae5
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-19 13:55:22 -0700

    checking alphabetical character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
 * @c: single letter input
 * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
 */
-int_isalpha(int c)
+int _isalpha(int c)
 {
 	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
