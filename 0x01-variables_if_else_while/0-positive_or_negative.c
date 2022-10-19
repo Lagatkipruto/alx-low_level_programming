ae1454cc312970efbf1077d796b886364a9cbabc
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-19 08:01:08 -0700

    assigning random numbers to the value

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <time.h>
 /**
  * main- program entry point.
- * return: 0-no error,non-zero value if error.
- **/
+ * Return: 0-no error,non-zero value if error.
+ */
 int main(void)
 {
 	int n;
