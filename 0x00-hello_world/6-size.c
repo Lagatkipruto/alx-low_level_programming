78f84b60be1304e338a71382ba4bb7c62b507188
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-18 19:29:35 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 /**
- * main-entry point for the program.
+ * main-Program entry point.
  *
- * Retun:zero if no error, return non-zero if errors.
+ * Retun: zero if no error, return non-zero if errors.
  */
 int main(void)
 {
@@ -12,5 +12,6 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+
 	return (0);
 }
