be111273936819d7486f7c03dbad904a26a8e827
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-20 09:09:23 -0700

    printing Fibonnic numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,	separeted by a comma followed by a space.
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int count;
-
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
 	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
-		printf("%lu", sum);
+		printf("%ld", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
