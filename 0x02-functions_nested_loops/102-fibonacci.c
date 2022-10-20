7bc6f57edb7b361dfe0f710c8e4472a6535e5467
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-20 09:13:49 -0700

    printing Fibonnic numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0; count < 50; count++)
+	for (count = 0; count <= 50; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%ld", sum);
