b9968b698c13fcbce56794b669fef9f357059628
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-20 20:11:10 -0700

    checking infinite loops

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,9 +2,9 @@
 
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)*/
-	/*{*/
-	/* putchar(i); */
-	/*}*/
+	while (i < 10)
+	{
+		putchar(i);
+	}
 
 	printf("Infinite loop avoided! \\o/\n");
 
