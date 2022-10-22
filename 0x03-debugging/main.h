740b666a0f721ac5900d6c33dbfe7880c9474a0e
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-22 10:32:01 -0700

    the calendar printing

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,11 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
+void positive_or_negative(int i);
+int convert_day(int month, int day);
+
+#endif /* main.h */
