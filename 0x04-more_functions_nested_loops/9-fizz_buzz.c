0ba508a1666b0a7392f65b94c0e21c9084dc1092
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-23 06:15:18 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,26 +14,24 @@ int main(void)
 	{
 
 		if ((i % 3 == 0) && (i % 5 == 0))
-		{
-
 			printf("FizzBuzz");
-		}
-		else if ((i % 3 == 0) && (i % 5 != 0))
-		{
 
+		else if ((i % 3) == 0)
+			printf("Fizz");
+
+		else if ((i % 5) == 0)
 			printf("Buzz");
-		}
+
 		else
-		{
+			printf("%d", i);
 
-			printf("%i", i);
-		}
-		if (i != 100)
-		{
 
-			printf(" ");
-		}
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
+
 	printf("\n");
+
 	return (0);
 }
