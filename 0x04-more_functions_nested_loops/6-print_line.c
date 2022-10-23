589d0ea9232867b52b779027dd8eef8a4c9a093f
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-23 03:48:02 -0700

    printing lines using _

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+
+	int len;
+
+	if (n > 0)
+	{
+
+		for (len = 0; len < n; len++)
+			putchar('_');
+	}
+
+	putchar('\n');
+}
