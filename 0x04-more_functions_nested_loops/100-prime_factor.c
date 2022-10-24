b6593f1ac40e2241e0e35b680af423220a2bf865
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-23 19:46:28 -0700

    printing the largest prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (SUcces)
+ */
+int main(void)
+{
+
+	long int n, test, prime, check;
+
+	n = 612852475143;
+	check = 0;
+	while (check == 0)
+	{
+
+		for (prime = 2; prime <= (n - 1); prime++)
+		{
+
+			test = n % prime;
+			if (test == 0)
+			{
+
+				break;
+			}
+		}
+		if (test == 0)
+		{
+
+			n = n / prime;
+		}
+		else
+		{
+
+			check = 1;
+		}
+	}
+	printf("%ld\n", n);
+	return (0);
+}
