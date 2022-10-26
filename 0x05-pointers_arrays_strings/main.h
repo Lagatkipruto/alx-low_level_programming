2f2ee128db90b3604319786af662997aeaedf864
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-25 22:52:06 -0700

    my main.h file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -15,5 +15,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 
 #endif
