611b4d968d221fe3a06cb3c67d906691ec5c1032
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-25 19:30:47 -0700

    writing a reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,13 @@ void print_rev(char *s)
 
 	int len = 0, index;
 
+
 	while (s[index++])
 		len++;
+
 	for (index = len - 1; index >= 0; index--)
 		putchar(s[index]);
+
 
 	putchar('\n');
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define main_h
 
 #include <stdlib.h>
+#include <string.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
