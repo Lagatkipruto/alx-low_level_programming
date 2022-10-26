faf57b622866ca57e9c41b0563684684a4f65cfa
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-25 23:35:53 -0700

    cracking passwords

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
  */
-void _puts(char *str)
+void puts(char *str)
 {
 
 	while (*str)
 		putchar(*str++);
 
-	putchar('\n');
+	_putchar('\n');
 }
