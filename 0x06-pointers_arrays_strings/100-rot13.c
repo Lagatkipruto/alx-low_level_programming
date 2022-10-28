2468c8aac9c051885805b1e12893ec75847bfb70
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-28 11:26:02 -0700

    encoding a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,7 +11,7 @@ char *rot13(char *str)
 
 	int indx1 = 0, indx2;
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		'G', 'H', 'I', 'J', 'K', 'L',
+			'G', 'H', 'I', 'J', 'K', 'L',
 			'M', 'N', 'O', 'P', 'Q', 'R',
 			'S', 'T', 'U', 'V', 'W', 'X',
 			'Y', 'Z', 'a', 'b', 'c', 'd',
@@ -28,6 +28,7 @@ char *rot13(char *str)
 		'x', 'y', 'z', 'a', 'b', 'c',
 		'd', 'e', 'f', 'g', 'h', 'i',
 		'j', 'k', 'l', 'm'};
+
 	while (str[indx1])
 	{
 
