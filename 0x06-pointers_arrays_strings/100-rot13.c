3cdd51a6479df1a2e74d1f996b870f6add1d0fb5
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-10-28 11:33:44 -0700

    function that encodes a sting using rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using rot13
+ * rot13 - Encodes a string using rot13.
  * @str: The string to be encoded.
  *
  * Return: A pointer to the encoded string.
