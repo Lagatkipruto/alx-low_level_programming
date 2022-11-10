399e6d01549ae4c586d490fcfe3411200b9d9af5
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-11-10 06:33:19 -0800

    Afunction that split a string int words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,13 +15,13 @@ char **strtow(char *str);
 int word_len(char *str)
 {
 
-int index = 0, len = 0;
+	int index = 0, len = 0;
 
-while (*(str + index) && *(str + index) != ' ')
+	while (*(str + index) && *(str + index) != ' ')
 	{
 		len++;
 		index++;
-}
+	}
 	return (len);
 }
 
