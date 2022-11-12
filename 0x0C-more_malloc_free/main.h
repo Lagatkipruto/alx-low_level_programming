41980ac5385d2a2e5e7d0e620ae8b41ba52ff3ee
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-11-12 00:08:59 -0800

    A function that allocates memory using malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
