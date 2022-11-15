fff4659df41efcbb60326c401069b011e9ab394e
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-11-15 07:41:20 -0800

    Header file that defines a macro named SIZE

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+* Desc: Header the defines a macro named
+*	SIZE as an abbreviation for the token 1024.
+*/
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This project is about preprocessor in C compiling.
