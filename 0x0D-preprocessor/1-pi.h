bd5186a7b10f21b30221f94d1a95830ca525dac2
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-11-15 07:55:42 -0800

    Header file that defines a macro named PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,15 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+* Fil: 1-pi.h
+* Author: Lagat Kipruto
+* Desc: Header file that defines a macro named PI as
+*	an abbreviation for the token 3.14159265359.
+*/
+
+#define PI 3.14159265359
+
+#endif
+
+
