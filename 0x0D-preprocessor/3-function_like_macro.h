60d8512ed5dfbca336312d7615ec3721ff2c9e4e
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-11-15 08:21:41 -0800

    A function-like macro ABS(x) that computes the absolute value of a number x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+* File: 3-function-like_macro.h
+* Author: Lagat Kipruto
+* Desc: Header file that defines a function-like macro ABS(x)
+*	that computes the absolute value of a number x.
+* @x: an absolute value of a number
+*/
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+
+#endif
+
