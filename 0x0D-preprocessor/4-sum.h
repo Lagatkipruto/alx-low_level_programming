c8e65b8cd81d4960b811b8366554d5a84bc1c270
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-11-15 08:31:43 -0800

    A function-like macro SUM(x, y)

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+* File: 4-sum.h
+* Author: Lagat Kipruto
+* Desc: Header file that defines a function-like macro SUM(x, y)
+*	that computes the sum of the numbers x and y.
+*/
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
