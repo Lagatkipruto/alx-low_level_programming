0aabb5e3581d1091574c0d987aeda24c623d93cb
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2022-11-28 12:23:05 -0800

    A function that frees a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,7 +3,7 @@
 
 /**
   * add_note - Adds a new node at the beginning
-  *		of a list.
+  *		of a list_t list.
   * @head: A pointer to the head of the list_t list.
   * @str: The string to be added to the list_t list.
   *
