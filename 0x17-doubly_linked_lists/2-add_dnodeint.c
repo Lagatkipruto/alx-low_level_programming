4eb1a46089d299abea83967eb874a1d7613a990c
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-01-12 08:51:03 -0800

    Double linke list in c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 
-	dlistint_t *new
+	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -19,6 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
+	new->next = *head;
 	if (*head != NULL)
 		(*head)->prev = new;
 	*head = new;
