57d23be57e8107102dae1c14f74112cdaed30c77
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-01-21 05:54:47 -0800

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * add - addition
+ *
+ *   * @a: number
+ *
+ *    * @b: number
+ *
+ *     * Return: result
+ *
+ *      */
+
+int add(int a, int b)
+
+{
+
+		return (a + b);
+
+}
+
+
+
+/**
+ *
+ *  * sub - subtraction
+ *
+ *   * @a: number
+ *
+ *    * @b: number
+ *
+ *     * Return: result
+ *
+ *      */
+
+int sub(int a, int b)
+
+{
+
+		return (a - b);
+
+}
+
+
+
+/**
+ *
+ *  * mul - multiplication
+ *
+ *   * @a: number
+ *
+ *    * @b: number
+ *
+ *     * Return: result
+ *
+ *      */
+
+int mul(int a, int b)
+
+{
+
+		return (a * b);
+
+}
+
+
+
+/**
+ *
+ *  * div - division
+ *
+ *   * @a: number
+ *
+ *    * @b: number
+ *
+ *     * Return: result
+ *
+ *      */
+
+
+
+int div(int a, int b)
+
+{
+
+		return (a / b);
+
+}
+
+/**
+ *
+ *  * mod - modulus
+ *
+ *   * @a: number
+ *
+ *    * @b: number
+ *
+ *     * Return: result
+ *
+ *      */
+
+int mod(int a, int b)
+
+{
+
+		return (a % b);
+
+}
